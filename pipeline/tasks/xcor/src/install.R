875161703b1da7d3c3237863d2ce688af96f8a63
Henry <hpratt111@live.com>
2018-11-03 13:47:32 -0400

    add xcor step

---
+++ pipeline/tasks/xcor/src/install.R
@@ -0,0 +1,5 @@
+install.packages("Rcpp", repos="http://cran.us.r-project.org")
+install.packages("caTools", repos="http://cran.us.r-project.org")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos="http://cran.us.r-project.org")
+BiocManager::install("Rsamtools", version = "3.8")
