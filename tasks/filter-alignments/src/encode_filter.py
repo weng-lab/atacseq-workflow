e2f1a59f50a69cccc9e1673550ec21550ea77027
Jason Brooks <jasonbrooks85@gmail.com>
2019-01-11 11:28:44 -0500

    Added tmp dir setting for filter-alignment task.

--- tasks/filter-alignments/src/encode_filter.py
+++ tasks/filter-alignments/src/encode_filter.py
@@ -169,12 +169,13 @@ def mark_dup_picard(bam, out_dir, prefix = "output"): # shared by both se and pe
     cmd += locate_picard()
     cmd += ' MarkDuplicates '
     # cmd = 'picard MarkDuplicates '
-    cmd += 'INPUT={} OUTPUT={} '
+    cmd += 'INPUT={} OUTPUT={} TMP_DIR={}'
     cmd += 'METRICS_FILE={} VALIDATION_STRINGENCY=LENIENT '
     cmd += 'ASSUME_SORTED=true REMOVE_DUPLICATES=false'
     cmd = cmd.format(
         bam,
         dupmark_bam,
+        out_dir,
         dup_qc)
     run_shell_cmd(cmd)
     return dupmark_bam, dup_qc
