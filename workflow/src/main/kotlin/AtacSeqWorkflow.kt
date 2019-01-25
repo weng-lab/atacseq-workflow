c1ea3f30423b89c3bd282c62192cb3aabc1bac74
Eugenio Mattei <emattei.phd@gmail.com>
2019-01-25 13:18:37 -0500

    - Force Single end Modification; Bam2Ta remove scaffolds

--- workflow/src/main/kotlin/AtacSeqWorkflow.kt
+++ workflow/src/main/kotlin/AtacSeqWorkflow.kt
@@ -10,8 +10,15 @@ data class AtacSeqParams(
         val samples: FastqSamples
 )
 
+/*data class MacsParams(
+        val samples: List<Macs2Input>
+)*/
+
 val atacSeqWorkflow = workflow("atac-seq-workflow") {
+
     val params = params<AtacSeqParams>()
+    val forceSingleEnd = true
+
 
     val trimAdaptorInputs = params.samples.replicates
             .map { TrimAdapterInput(it) }
@@ -27,10 +34,16 @@ val atacSeqWorkflow = workflow("atac-seq-workflow") {
     val filterTask = filterTask(filterInput)
 
     val bam2taInput = filterTask.outputPub
-            .map { Bam2taInput(it.bam, it.repName, it.pairedEnd) }
+            .map { Bam2taInput(it.bam, it.repName, if(forceSingleEnd) false else it.pairedEnd) }
     val bam2taTask = bam2taTask(bam2taInput)
 
     val macs2Input = bam2taTask.outputPub
             .map { Macs2Input(it.ta, it.repName, it.pairedEnd) }
     macs2Task(macs2Input)
+
+    /*
+    val params = params<MacsParams>()
+    val macs2Input = params.samples.map {Macs2Input(it.ta,it.repName,it.pairedEnd)}.toFlux()
+    macsTask(macs2Input)
+    */
 }
