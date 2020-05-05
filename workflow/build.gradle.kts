56357a0a576f062afda13aba70f730100a83b7d5
NishiPhalke <nishaphalke@gmail.com@users.noreply.github.com>
2020-05-05 13:39:01 -0400

    bumping macs2 task version

--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.genomealmanac.atacseq"
-version = "0.1.7"
+version = "0.1.8"
 val artifactID = "atacseq-workflow"
 
 repositories {
--- workflow/src/main/kotlin/task/Macs2.kt
+++ workflow/src/main/kotlin/task/Macs2.kt
@@ -32,7 +32,7 @@ data class Macs2Output(
 fun WorkflowBuilder.macs2Task(i: Publisher<Macs2Input>) = this.task<Macs2Input, Macs2Output>("macs2", i) {
     val params = taskParams<Macs2Params>()
 
-    dockerImage = "genomealmanac/atacseq-macs2:1.0.6"
+    dockerImage = "genomealmanac/atacseq-macs2:1.0.4"
 
     val prefix = "macs2/${input.repName}"
     val npPrefix = "$prefix.pval${params.pvalThresh}.${capNumPeakFilePrefix(params.capNumPeak)}"
