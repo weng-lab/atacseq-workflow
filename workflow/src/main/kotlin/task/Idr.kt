b2c3a3c0b9b860869f9e76ac84855cb100700267
Thomas Reimonn <thomas.reimonn@umassmed.edu>
2020-11-19 09:44:22 -0500

    Fixed missing idr bigBed, fixed tsse write to / issue

--- workflow/src/main/kotlin/task/Idr.kt
+++ workflow/src/main/kotlin/task/Idr.kt
@@ -40,7 +40,7 @@ fun WorkflowBuilder.idrTask(i: Publisher<IdrInput>, peak: String) = this.task<Id
             IdrOutput(
                     npeak = OutputFile("$nPrefix.narrowPeak.gz"),
                     bfiltNpeak = OutputFile("$nPrefix.bfilt.narrowPeak.gz"),
-                    bfiltNpeakBB = OutputFile("$prefix.bfilt.narrowPeak.bb"),
+                    bfiltNpeakBB = OutputFile("$nPrefix.bfilt.narrowPeak.bb"),
                     fripQc = OutputFile("$nPrefix.bfilt.frip.qc"),
                     idrPlot = OutputFile("$nPrefix.unthresholded-peaks.txt.png"),
                     idrUnthresholdedPeak = OutputFile("$nPrefix.unthresholded-peaks.txt.gz")
